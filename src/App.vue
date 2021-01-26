0ae9c4d407291f8e8cdd2d3d151543f0bf681c1e
unknown <marcel@BTSSIO.LOCAL>
2021-01-26 12:01:52 +0100

    a

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
         elevation="2"
         style="color: blue; width: 200px; background: grey; margin-left: auto; margin-right: auto"
     >Appuyez moi desus svp</v-btn>
+
   </v-app>
 </template>
 
