6e4b8565a89fcb863a9ca3a846e3a44cc2c1bb23
unknown <marcel@BTSSIO.LOCAL>
2021-01-27 16:47:56 +0100

    Modification

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <v-app>
 
+      <h1 style="text-align: center; margin-top: 10px">
+        Salut
+      </h1>
+
+
     <v-btn
         elevation="2"
-        style="color: blue; width: 200px; background: grey; margin-left: auto; margin-right: auto"
-    >Appuyez moi desus svp</v-btn>
+        style="color: blue; width: 210px; background: grey; margin-left: auto; margin-right: auto"
+    >Appuyez moi dessus svp</v-btn>
 
   </v-app>
 </template>
